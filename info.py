a2e584f148dfda3c0879de21a8f693f94b7b46c5
Anantha Ramu. K <142468482+anantharamu15@users.noreply.github.com>
2024-05-24 19:22:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -26,8 +26,8 @@ ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002122293467').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-auth_channel = environ.get('AUTH_CHANNEL')
-auth_grp = environ.get('AUTH_GROUP','-1002122293467')
+auth_channel = environ.get('AUTH_CHANNEL',-1002131808231)
+auth_grp = environ.get('AUTH_GROUP')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
@@ -52,8 +52,8 @@ BUTTON_LOCK = is_enabled(environ.get("BUTTON_LOCK", "True"), True)
 RemoveBG_API = environ.get("RemoveBG_API", "")
 
 # url shortner
-SHORT_URL = environ.get("SHORT_URL", "http://krownlinks.com")
-SHORT_API = environ.get("SHORT_API", "a8090857d8890b0f9c025db47b5697c2328d4444")
+SHORT_URL = environ.get("SHORT_URL", "http://gyanilinks.com")
+SHORT_API = environ.get("SHORT_API", "76f21de69c69cbd8df3b91f0c00c4d90e0a664a1")
 
 # Others
 IMDB_DELET_TIME = int(environ.get('IMDB_DELET_TIME', "300"))
